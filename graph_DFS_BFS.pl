4241b15e4c1f5741f322a7e961c43c378361fd54
oreillyj-cse <39863560+oreillyj-cse@users.noreply.github.com>
2021-05-19 11:35:29 -0400

    add graph

---
+++ graph_DFS_BFS.pl
@@ -0,0 +1,23 @@
+%DAG
+e(a,c).
+e(a,g).
+e(c,g).
+e(a,d).
+e(d,g).
+e(d,h).
+e(g,h).
+e(f,b).
+e(f,e).
+e(b,e).
+e(e,a).
+
+%cyclic graph
+% e(a,a).
+% e(a,b). e(a,e). e(a,f).
+% e(b,c). e(b,e).
+% e(c,d). e(c,e).
+% e(e,d).
+% e(f,e). e(f,g). e(f,h).
+% e(g,d).
+% e(h,g). e(h,i).
+% e(i,f). e(i,a).
